822cf7a9170850fa3176130bd5bb1fa7338f4035
voorevamshi <vvamshijava@gmail.com>
2026-08-04 07:39:42 +0530

    Added first program

---
+++ src/main/java/com/vmc/dsa/reversepattern/ReverseArray.java
@@ -0,0 +1,26 @@
+package com.vmc.dsa.reversepattern;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		char[] arr= {'a','b','c','d','e'};
+		reverse(arr,0,4);
+		
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+	private static void reverse(char[] arr, int low, int high) {
+		
+		while(low<high) {
+			char temp=arr[low];
+			arr[low]=arr[high];
+			arr[high]=temp;
+			low++;
+			high--;
+		}
+	}
+
+}
